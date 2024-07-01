d95b3f521a8bf2265bd6498872128c21d6c880c0
htetgit <htetyemon@galaxie.ai>
2024-07-01 14:33:46 +0630

    Initial commit with Hello World app

--- main.go
+++ main.go
@@ -1,19 +1,8 @@
 // main.go
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
-
-	fmt.Println("Server is listening on port 8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Error starting server:", err)
-	}
+	fmt.Println("Hello, World!")
 }
